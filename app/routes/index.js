58499adcf4a723564af89ae262bf98da7b882b2b
Alex Wohlbruck <alexwohlbruck@gmail.com>
2017-03-11 21:28:07 +0000

    Improved search and playback mechanisms

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,6 +3,8 @@ var router = express.Router();
 var path = require('path');
 
 router.use('/users', require('./user.routes'));
+router.use('/search', require('./search.routes'));
+router.use('/albums', require('./album.routes'));
 router.use('/tracks', require('./track.routes'));
 
 module.exports = router;
--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -2,8 +2,17 @@ var express = require('express');
 var router = express.Router();
 var spotify = require.main.require('./app/services/spotify');
 
-router.get('/:user/playlists/:playlistId', function(req, res) {
-	spotify.getPlaylist(req.params.user, req.params.playlistId)
+router.get('/:user/playlists/:playlistID', function(req, res) {
+	spotify.getPlaylist(req.params.user, req.params.playlistID)
+	  .then(function(data) {
+	  	res.status(200).json(data.body);
+	  }, function(err) {
+	    res.status(400).json(err);
+	  });
+});
+
+router.get('/:user/playlists/:playlistID/tracks', function(req, res) {
+	spotify.getPlaylistTracks(req.params.user, req.params.playlistID)
 	  .then(function(data) {
 	  	res.status(200).json(data.body);
 	  }, function(err) {
